abadbe4fa007efcb637549c1272656ca788a4b69
Dane Weber <DaneWeber@users.noreply.github.com>
2022-10-26 13:33:46 -0400

    gigalixir build configuration is extended
    - Following directions from https://blog.miguelcoba.com/deploying-an-elixir-release-to-gigalixir

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("APP_NAME") <> ".gigalixirapp.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :card_game_alpha, CardGameAlphaWeb.Endpoint,
